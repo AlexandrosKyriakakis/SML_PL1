bb6a202e30947176d7c53e551b36b56fad8f540a
hmmyalex@gmail.com <hmmyalex@gmail.com>
2020-03-26 21:50:03 +0200

    fixed git bash script

--- git.sh
+++ git.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 git add .
 git commit -m "$1"
-git push -u origin $2
 
 str=$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
 subStr=${str:7:100}
-echo $subStr
+
+git push -u origin $subStr
+
 
 
